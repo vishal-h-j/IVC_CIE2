5c744d0f09e20d26fdbd33ed883f5694e5c95f84
Vishal H J <vishalhj453@gmail.com>
2026-03-26 12:12:31 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+class_name = input("Enter your class: ")
+section = input("Enter your section: ")
+usn = input("Enter your USN: ")
+
+print("\nHello, my name is", name)
+print("I am from class", class_name)
+print("Section", section)
+print("USN", usn)
